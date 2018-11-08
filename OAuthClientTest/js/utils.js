4c67f18d721a7be846006c25d95a082f14e62c3c
Ruben de Laat <ruben@logic-labs.nl>
2018-11-08 20:28:44 +0100

    - Added ability to download bot output - Default endPoint is now the current host

--- OAuthClientTest/js/utils.js
+++ OAuthClientTest/js/utils.js
@@ -22,6 +22,28 @@ var QueryString = function () {
 	}();
 	
 
+	// Got this from https://blog.centerkey.com/2013/05/javascript-colorized-pretty-print-json.html
+	var jsonPrettyPrint = {
+		replacer : function(match, pIndent, pKey, pVal, pEnd) {
+			var key = '<span class=json-key>';
+			var val = '<span class=json-value>';
+			var str = '<span class=json-string>';
+			var r = pIndent || '';
+			if (pKey)
+				r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+			if (pVal)
+				r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+			return r + (pEnd || '');
+		},
+		toHtml : function(obj) {
+			var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+			return JSON.stringify(obj, null, 3).replace(/&/g, '&amp;')
+					.replace(/\\"/g, '&quot;').replace(/</g, '&lt;')
+					.replace(/>/g, '&gt;').replace(jsonLine,
+							jsonPrettyPrint.replacer);
+		}
+	};
+	
 function post(url, data, callback, errorCallback) {
 	var settings = {
 		contentType: "application/json",
